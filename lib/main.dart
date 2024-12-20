10d581ce180e1d005363d7bd570c49d6adbb8c2e
adarsa <asdf@asdf.com>
2024-12-20 18:51:33 +0545

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:gym_tracker_app/view/homepage_view.dart';
+import 'package:gym_tracker_app/common/app_theme/app_theme.dart';
+import 'package:gym_tracker_app/view/bottom_navigation_screens/dashboard_view.dart';
 import 'package:gym_tracker_app/view/login_view.dart';
 import 'package:gym_tracker_app/view/onboarding_view.dart';
 import 'package:gym_tracker_app/view/signup_view.dart';
 import 'package:gym_tracker_app/view/splash_screen_view.dart';
 
 void main() {
-  runApp(GymTrackerApp());
+  runApp(const GymTrackerApp());
 }
 
 class GymTrackerApp extends StatelessWidget {
+  const GymTrackerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
       initialRoute: '/',
       routes: {
-        '/': (context) => SplashScreen(),
+        '/': (context) => const SplashScreen(),
         '/onboarding': (context) => OnboardingScreen(),
         '/signup': (context) => SignupPage(),
         '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
+        '/home': (context) => const MyDashboardView(),
       },
     );
   }
