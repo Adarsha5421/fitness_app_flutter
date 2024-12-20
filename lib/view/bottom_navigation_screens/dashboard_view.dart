032807f4cd4e7414ac63bbe9f79d229902800549
adarsa <asdf@asdf.com>
2024-12-20 18:35:57 +0545

    bottom navigation

---
+++ lib/view/bottom_navigation_screens/dashboard_view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:gym_tracker_app/view/bottom_navigation_screens/group_view.dart';
+import 'package:gym_tracker_app/view/bottom_navigation_screens/notification_view.dart';
+import 'package:gym_tracker_app/view/bottom_navigation_screens/profile_view.dart';
+import 'package:gym_tracker_app/view/bottom_navigation_screens/workout_view.dart';
+import 'package:gym_tracker_app/view/homepage_view.dart';
+
+class MyDashboardView extends StatefulWidget {
+  const MyDashboardView({super.key});
+
+  @override
+  State<MyDashboardView> createState() => _MyDashboardViewState();
+}
+
+class _MyDashboardViewState extends State<MyDashboardView> {
+  int _selectedIndex = 0;
+  List<Widget> lstBOttomScreen = [
+    const HomePage(),
+    const GymScreen(),
+    const GroupScreen(),
+    const ProfileScreen(),
+    const NotificationScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Dashboard',
+          ),
+          backgroundColor: Colors.pink,
+          centerTitle: true,
+        ),
+        body: lstBOttomScreen[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          backgroundColor: Colors.pink,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.run_circle_sharp),
+              label: 'Workout',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.group_sharp,
+                ),
+                label: 'Group'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                ),
+                label: 'Profile'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.notification_add_outlined,
+                ),
+                label: 'Notification'),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
