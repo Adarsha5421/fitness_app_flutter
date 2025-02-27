dfc99e406a671b1d5d5e057dc020a51c4fb98412
sandesh1417 <sandeshshrestha017@gmail.com>
2025-02-27 15:56:32 +0545

    FEAT :: services added

---
+++ lib/core/api_endpoints.dart
@@ -0,0 +1,13 @@
+class APIEndPoints {
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
+
+
+  static String baseUrl = "http://localhost:5000/api/";
+  static String loginUrl = "auth/login";
+  static String signUpUrl = "auth/signup";
+  static String meUrl = "auth/me";
+  static String logOutUrl = "auth/logout";
+
+}
